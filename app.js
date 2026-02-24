0171426e8c4920e1c53653f7fed69468795ca276
Nischith <nischith19.bm@gmail.com>
2026-02-24 10:41:24 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,41 @@
+let defaultval="avenger"
+
+function findmovie(){
+    let searchmovie =document.getElementById("searchhere");
+    let searchval=searchmovie.value;
+    defaultval = searchmovie.value;
+    defaultval=searchval;
+    console.log(searchval);
+    console.log(defaultval)
+    getMovie();
+    document.getElementById("results").scrollIntoView({
+        behavior: "smooth"
+    });
+}
+async function getMovie() {
+    let movies= await fetch(`http://www.omdbapi.com/?s=${defaultval}&apikey=8ad03308`);
+    movies = await movies.json();
+    let omdMovies=document.getElementById("showmoviedetails")
+    omdMovies.innerHTML="";
+    if(movies.Response=="False"){
+        omdMovies.innerHTML=`<h2>${movies.Error}</h2>`
+        return;
+    }
+    movies.Search.map((movie)=>{
+        return omdMovies.innerHTML+=`
+        <div class="moviecard">
+        <div><h3>${movie.Title}</h3></div>
+        <div class ="movieimg">
+        <img src="${movie.Poster=="N/A"?" ./no_image_available.png":movie.Poster}">
+        </div>
+        <div>${movie.Year}</div>
+        </div>
+        `;
+    });
+    document.getElementById("results").scrollIntoView({
+        behavior: "smooth"
+    });
+
+}
+getMovie();
+
